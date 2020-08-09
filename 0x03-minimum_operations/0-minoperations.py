2c62263a7b39d61bfd62ad1f88e776070aa73e10
andreammgcol <amendez72@misena.edu.co>
2020-08-09 17:07:21 -0500

    Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+
+def esPar(x):
+    return x % 2 == 0
+
+
+def minOperations(n):
+
+    fileText = 'H'
+    clipboard = ''
+    numOper = 0
+    index = 1
+
+    while len(fileText) < n:
+        if esPar(n):
+            # logica de mitades
+            if len(fileText) < (n / 2):
+                clipboard = fileText
+                fileText += clipboard
+                numOper += 2
+            else:
+                fileText += clipboard
+                numOper += 1
+        else:
+            # logic por multi
+            if n % index == 0:
+                clipboard = fileText
+                fileText += clipboard
+                numOper += 2
+            else:
+                fileText += clipboard
+                numOper += 1
+        index += 1
+
+    return numOper
