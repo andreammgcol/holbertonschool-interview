7fcd129156b43bf4b10e6216fe208b7d04687b85
andreammgcol <amendez72@misena.edu.co>
2020-08-10 02:37:11 -0500

    simplifying validation

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,31 +9,16 @@ def minOperations(n):
     H = 1
     clipboard = 0
     numOper = 0
-    index = 1
 
     if type(n) is int and n >= 2:
         while H < n:
-            if n % 2 == 0:
-                # logica de mitades
-                if H < (n / 2):
-                    clipboard = H
-                    H += clipboard
-                    numOper += 2
-                else:
-                    H += clipboard
-                    numOper += 1
+            if n % H == 0:
+                clipboard = H
+                H += clipboard
+                numOper += 2
             else:
-                # logic por multi
-                if n % index == 0:
-                    clipboard = H
-                    H += clipboard
-                    numOper += 2
-                else:
-                    H += clipboard
-                    numOper += 1
-            index += 1
-
+                H += clipboard
+                numOper += 1
         return numOper
-
     else:
         return 0
