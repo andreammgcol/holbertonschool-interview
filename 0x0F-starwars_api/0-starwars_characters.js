5d84d28486322e8c1eb5fae417fbd30a23c8a2d3
andreammgcol <amendez72@misena.edu.co>
2021-04-07 17:19:30 -0500

    fixing indentation and spaces

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,23 +3,23 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-let firstName = function(){
-    request(url, function(error, response, body){
-        if (error) throw error;
-        let i = 0;
-        secondName(JSON.parse(body).characters, i);
-    });
+let firstName = function () {
+  request(url, function (error, response, body) {
+    if (error) throw error;
+    let i = 0;
+    secondName(JSON.parse(body).characters, i);
+  });
 };
 
-let secondName = function(characters, i){
-    if (characters.length === i) {
-        return;
-    }
-    request(characters[i], function (error, response, body){
-        if (error) throw error;
-        console.log(JSON.parse(body).name);
-        secondName(characters, ++i);
-    });
+let secondName = function (characters, i) {
+  if (characters.length === i) {
+    return;
+  }
+  request(characters[i], function (error, response, body) {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    secondName(characters, ++i);
+  });
 };
 
 firstName();
