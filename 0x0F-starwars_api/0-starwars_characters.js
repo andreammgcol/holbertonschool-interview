34599e3c0a506a69e8123faffe14d78282554816
andreammgcol <amendez72@misena.edu.co>
2021-04-09 12:27:20 -0500

    improve code requirements

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -6,8 +6,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const firstName = function () {
   request(url, function (error, response, body) {
     if (error) throw error;
-    let i = 0;
-    secondName(JSON.parse(body).characters, i);
+    secondName(JSON.parse(body).characters, 0);
   });
 };
 
