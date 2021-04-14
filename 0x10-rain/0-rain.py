ea38153d6f4f05e72939f12727d550a64cffc3ee
andreammgcol <amendez72@misena.edu.co>
2021-04-14 16:18:02 -0500

    first commit

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def rain(walls):
+    """Calculate how much water will be retained after it rains
+    """
+    n = len(walls)
+    amount = 0
+
+    if walls is None:
+        return 0
+
+    for i in range(1, n - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        right = walls[i]
+        for j in range(i + 1, n):
+            right = max(right, walls[j])
+
+        amount = amount + (min(left, right) - walls[i])
+
+    return amount
