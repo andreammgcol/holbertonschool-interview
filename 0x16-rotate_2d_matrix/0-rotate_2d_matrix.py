8db36546772c9ad5a48402871ca1698782c84820
andreammgcol <amendez72@misena.edu.co>
2021-05-25 19:48:24 -0500

    first commit

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Rotate 2D Matrix
+"""
+def rotate_2d_matrix(matrix):
+    """ Rotate """
+    matrix.reverse()
+    temp = [row.copy() for row in matrix]
+
+    for x, row in enumerate(temp):
+        for y, n in enumerate(row):
+            matrix[y][x] = temp[x][y]
